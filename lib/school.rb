deaf841a6f518c702ad7860fc8c322b6e0bd0c50
pj <pjhg14@gmail.com>
2021-04-27 22:21:23 -0400

    commit tree f5e8aafbf2cf8abf826d27fb1ace807e656660b2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+
+    attr_reader :roster, :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+        end
+
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.keys.each do |key|
+            @roster[key].sort!
+        end
+
+        @roster
+    end
+end
